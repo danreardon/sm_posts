b655b4f70c7c715c3d2c4304c673ef6fa95ac0d5
Daniel Reardon <drdanreardon@gmail.com>
2011-12-05 14:43:56 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmPosts::Application.config.secret_token = '71394e7bb6005307da159129b652e03867ab022033ad045df09f5fcbae888ac52866a28c5e27628ec20a11cf43e5a914c1a99b6105b470957a39752fac6f9b3a'
